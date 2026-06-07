0b6f85fe23f8f71d73e684f50442b3cf60748fe0
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-06-07 21:30:19 +0300

    Declare ecr repo name variable.

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+}
