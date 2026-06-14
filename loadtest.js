ca50a4a89fec715855c89cce65b6a29c31b31e0d
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-06-14 21:27:40 +0300

    Increase users to 200 and reduce sleep to 0.1.

--- loadtest.js
+++ loadtest.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 50,
+  vus: 200,
   duration: '3m',
 };
 
 export default function () {
   http.get('http://k8s-default-gitea-278ff7d493-1549881267.eu-central-1.elb.amazonaws.com');
-  sleep(0.5);
+  sleep(0.1);
 }
