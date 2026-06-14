41b6bb60b3b3d0b6a0a58677505a4309f90e2a1c
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-06-14 21:16:23 +0300

    Add load test for HPA.

---
+++ loadtest.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 50,
+  duration: '3m',
+};
+
+export default function () {
+  http.get('http://k8s-default-gitea-278ff7d493-1549881267.eu-central-1.elb.amazonaws.com');
+  sleep(0.5);
+}
